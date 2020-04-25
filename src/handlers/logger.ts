24e3529904441354a244ec02b4eba43a72e7774a
Karolis Uzkuraitis <karolis.uzkuraitis+github@gmail.com>
2020-04-25 23:53:21 +0300

    added logger levels configuration

--- src/handlers/logger.ts
+++ src/handlers/logger.ts
@@ -1,14 +1,40 @@
+declare global {
+    interface Window {
+        logLevel?: unknown;
+    }
+}
+
+enum LogLevel {
+    error,
+    warn,
+    info,
+}
+
 class Logger {
-    public log(a: string) {
-        console.log(a);
+    public isLogLevelEnabled(level: LogLevel): boolean {
+        if (typeof window.logLevel === 'number') {
+            return window.logLevel >= level;
+        }
+        window.logLevel = 0;
+        return 0 >= level;
     }
 
-    public logWarn(a: string) {
-        console.warn(a);
+    public log(a: string): void {
+        if (this.isLogLevelEnabled(LogLevel.info)) {
+            console.log(a);
+        }
     }
 
-    public logError(a: string) {
-        console.error(a);
+    public logWarn(a: string): void {
+        if (this.isLogLevelEnabled(LogLevel.warn)) {
+            console.warn(a);
+        }
+    }
+
+    public logError(a: string): void {
+        if (this.isLogLevelEnabled(LogLevel.error)) {
+            console.error(a);
+        }
     }
 }
 
