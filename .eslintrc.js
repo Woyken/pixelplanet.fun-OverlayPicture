4d2178bb3bd5c408b64eed58e50ef3a5afe4c9d2
Karolis Uzkuraitis <karolis.uzkuraitis+github@gmail.com>
2020-04-26 13:14:06 +0300

    require function return types

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,5 +22,6 @@ module.exports = {
         // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
         // e.g. "@typescript-eslint/explicit-function-return-type": "off",
         '@typescript-eslint/no-empty-interface': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'error'
     },
 };
