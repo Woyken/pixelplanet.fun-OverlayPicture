607c2091a61e132a9b2e5c0dcb0a0785aa4b5577
Woyken <Woyken@users.noreply.github.com>
2020-05-03 12:03:52 +0000

    deploy: 0988737aad98f772106c94c3877336e9ac7b6613

---
+++ index.js
@@ -0,0 +1,4 @@
+var unused = 0;
+// This file is only used for rollup build process...
+
+export { unused };
---
+++ pixelPlanetOverlay-loader.user.js
@@ -0,0 +1,13 @@
+
+// ==UserScript==
+// @name         pixelplanet.fun picture overlay
+// @namespace    https://github.com/Woyken/pixelplanet.fun-OverlayPicture
+// @version      1.0.17
+// @description  Add your picture as overlay to pixelplanet.fun
+// @author       Woyken
+// @include      https://pixelplanet.fun/*
+// @grant        none
+// @downloadURL  https://woyken.github.io/pixelplanet.fun-OverlayPicture/pixelPlanetOverlay-loader.user.js
+// ==/UserScript==
+
+!function(){"use strict";var e=document.createElement("script");e.src="https://woyken.github.io/pixelplanet.fun-OverlayPicture/pixelPlanetOverlay.user.js",document.body.appendChild(e)}();
