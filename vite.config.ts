d1e5c8e04d2d916eb2bddf613f909f3a3a4e0d7c
Karolis Uzkuraitis <karolis.uzkuraitis+github@gmail.com>
2022-02-21 03:29:33 +0200

    fiddling with build parameters

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,45 @@ import { resolve } from 'path';
 import { visualizer } from 'rollup-plugin-visualizer';
 import webWorkerLoader from 'rollup-plugin-web-worker-loader';
 import { UserConfig } from 'vite';
+import banner from 'vite-plugin-banner';
 import Checker from 'vite-plugin-checker';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 import react from '@vitejs/plugin-react';
 
+import pkg from './package.json';
+
+const loaderModuleBanner = `
+// ==UserScript==
+// @name         ${pkg.displayName}
+// @namespace    https://github.com/Woyken/pixelplanet.fun-OverlayPicture
+// @version      ${pkg.version}
+// @description  ${pkg.description}
+// @author       ${pkg.author}
+// @include      https://pixelplanet.fun/*
+// @grant        none
+// @downloadURL  https://woyken.github.io/pixelplanet.fun-OverlayPicture/pixelPlanetOverlay-loader.user.js
+// ==/UserScript==
+/**/
+`;
+
+const moduleBanner = `
+// ==UserScript==
+// @name         ${pkg.displayName}
+// @namespace    https://github.com/Woyken/pixelplanet.fun-OverlayPicture
+// @version      ${pkg.version}
+// @description  ${pkg.description}
+// @author       ${pkg.author}
+// @include      https://pixelplanet.fun/*
+// ==/UserScript==
+// This script is supposed to be loaded by https://woyken.github.io/pixelplanet.fun-OverlayPicture/pixelPlanetOverlay-loader.user.js module.
+/**/
+`;
 
 function pathResolve(dir: string) {
     return resolve(__dirname, '.', dir);
 }
-
+const isLoaderBuild = process.env.APP_MODULE === 'loader';
 const mode = process.env.APP_ENV;
 const isDev = mode === 'development';
 
@@ -21,22 +50,22 @@ const isDev = mode === 'development';
 const config: () => UserConfig = () => ({
     mode,
     build: {
-        lib: { entry: 'src/index.tsx', formats: ['iife'], name: 'userscript-bbuild-with-vite.js' },
+        emptyOutDir: false,
+        lib: isLoaderBuild
+            ? {
+                  fileName: () => 'pixelPlanetOverlay-loader.user.js',
+                  entry: 'src/userscript-loader-module/pixelPlanetOverlay-loader.ts',
+                  formats: ['iife'],
+                  name: 'userscript-loader-build-with-vite.js',
+              }
+            : { fileName: () => 'pixelPlanetOverlay.user.js', entry: 'src/index.tsx', formats: ['iife'], name: 'userscript-build-with-vite.js' },
         sourcemap: isDev ? 'inline' : false,
         minify: isDev ? false : undefined,
         rollupOptions: {
+            output: {
+                extend: true,
+            },
             plugins: [webWorkerLoader(), isDev ? visualizer({ filename: 'moduleVisualizerOutput.html' }) : undefined],
-            // output: {
-            //     manualChunks: (id, api): string | undefined => {
-            //         if (id.includes('node_modules')) {
-            //             if (id.includes('@mui')) return 'vendor_mui';
-            //             if (id.includes('pako')) return 'vendor_pako';
-            //             if (id.includes('i18next')) return 'vendor_i18next';
-            //             return 'vendor'; // all other package goes here
-            //         }
-            //         return undefined;
-            //     },
-            // },
         },
     },
     resolve: {
@@ -48,6 +77,7 @@ const config: () => UserConfig = () => ({
         ],
     },
     plugins: [
+        banner(isLoaderBuild ? loaderModuleBanner : moduleBanner),
         tsconfigPaths(),
         react({
             jsxRuntime: isDev ? 'classic' : undefined,
@@ -78,11 +108,7 @@ const config: () => UserConfig = () => ({
         }),
         Checker({
             typescript: true,
-            overlay: true,
-            // eslint: {
-            //     files: 'src',
-            //     extensions: ['.ts', '.tsx'],
-            // },
+            overlay: false,
         }),
     ],
 });
