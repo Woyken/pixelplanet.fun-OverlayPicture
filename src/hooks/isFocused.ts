6cba6f4e6270519c1ae87af9ac36f84864ef2032
Karolis Uzkuraitis <karolis.uzkuraitis+github@gmail.com>
2022-02-23 22:58:36 +0200

    unused import

--- src/hooks/isFocused.ts
+++ src/hooks/isFocused.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useState } from 'react';
 
 export const useIsFocused = () => {
     const [isFocused, setIsFocused] = useState(false);
