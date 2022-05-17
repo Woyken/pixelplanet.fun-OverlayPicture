74e5f1aaffaaf26282eba19ccccac9548db214d1
Karolis Užkuraitis <karolis.uzkuraitis+github@gmail.com>
2022-05-17 20:01:27 +0300

    temp v2

---
+++ v2/pixelPlanetOverlay-loader.user.js
@@ -0,0 +1,23 @@
+
+// ==UserScript==
+// @name         pixelplanet.fun picture overlay
+// @namespace    https://github.com/Woyken/pixelplanet.fun-OverlayPicture
+// @version      1.1.0
+// @description  Add your picture as overlay to pixelplanet.fun
+// @author       Woyken
+// @include      https://pixelplanet.fun/*
+// @grant        none
+// @downloadURL  https://woyken.github.io/pixelplanet.fun-OverlayPicture/v2/pixelPlanetOverlay-loader.user.js
+// ==/UserScript==
+/**/
+
+{
+    const e = document.createElement("script");
+    if (window.location.host.startsWith("localhost"))
+      e.src = new URL("src/userscript-loader-module/pixelPlanetOverlay.user.ts", window.location.href).href;
+    else
+      e.src = "https://woyken.github.io/pixelplanet.fun-OverlayPicture/v2/pixelPlanetOverlay.user.js";
+    e.type = "module";
+    document.body.appendChild(e);
+  }
+  
