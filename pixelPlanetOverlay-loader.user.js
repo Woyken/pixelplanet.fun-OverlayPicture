a0d3c54acc51f84c9346965a2fc09c3d87ebeb47
Woyken <Woyken@users.noreply.github.com>
2022-05-17 18:52:20 +0000

    deploy: 4615d8580f3f32b48fe07199c27a1232a7643f37

--- pixelPlanetOverlay-loader.user.js
+++ pixelPlanetOverlay-loader.user.js
@@ -16,7 +16,7 @@
   if (window.location.host.startsWith("localhost"))
     e.src = new URL("src/userscript-loader-module/pixelPlanetOverlay.user.ts", window.location.href).href;
   else
-    e.src = "https://woyken.github.io/pixelplanet.fun-OverlayPicture/assets/pixelPlanetOverlay.user.js";
+    e.src = "https://woyken.github.io/pixelplanet.fun-OverlayPicture/pixelPlanetOverlay.user.js";
   e.type = "module";
   document.body.appendChild(e);
 }
