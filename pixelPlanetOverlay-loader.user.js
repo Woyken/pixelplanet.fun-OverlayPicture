a11ea9444b6a7e92b2a4905a97aec85ee27c3ddf
Woyken <Woyken@users.noreply.github.com>
2022-09-07 10:33:09 +0000

    deploy: 2b8a2237f38c57860df593fd4c89e0cf1d6a0d10

--- pixelPlanetOverlay-loader.user.js
+++ pixelPlanetOverlay-loader.user.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name         pixelplanet.fun picture overlay
 // @namespace    https://github.com/Woyken/pixelplanet.fun-OverlayPicture
-// @version      1.1.7
+// @version      1.1.8
 // @description  Add your picture as overlay to pixelplanet.fun
 // @author       Woyken
 // @include      https://pixelplanet.fun/*
